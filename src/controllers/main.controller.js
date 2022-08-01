0a228a3879172a33c0ef86b339a83967583f7c76
nachog00 <ignaciog00@gmail.com>
2022-07-31 21:02:47 -0300

    added the admin user filter

--- src/controllers/main.controller.js
+++ src/controllers/main.controller.js
@@ -1,12 +1,20 @@
 /* require's */
 const fs = require('fs');
+const { resolve } = require('path');
 const path = require('path');
 
 /* cotrollers definitions */
 const controller = {
     
-    index : (req, res) => {
-        res.send( 'Hi there')
+    index : ( req, res) => {
+        const admins = [ "Ada","Greta","Vim","Tim"];
+        const user = req.query.user;
+
+        admins.some( admin => admin.toLowerCase() == user.toLowerCase() ) ?
+            res.send( `Hola admin: ${user}` ) :
+            res.send( 'No tienes permiso para ver este contenido.');
+
+
     }
 
 }
